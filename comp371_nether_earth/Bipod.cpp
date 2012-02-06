48a0d43e0c98e6addca0897bbf10fc0632e11f37
Taras Kamtchatnikov <eggmik@hotmail.com>
2012-02-06 16:04:19 -0500

    Added team member names to robot leg files.

--- comp371_nether_earth/Bipod.cpp
+++ comp371_nether_earth/Bipod.cpp
@@ -1,3 +1,13 @@
+//Comp371_w12 Section R
+//Prof. S. Mokhov
+//Programming Assignment 1
+//Team 4
+//Jordan V. 1300520
+//Taras K. 6901204
+//Gianni T. 1938878
+//Sebastien S. 9500782
+//This is the Bipod implimentation file.
+
 #include "Bipod.h"
 #include <GL\glut.h>
 
--- comp371_nether_earth/Bipod.h
+++ comp371_nether_earth/Bipod.h
@@ -1,3 +1,14 @@
+//Comp371_w12 Section R
+//Prof. S. Mokhov
+//Programming Assignment 1
+//Team 4
+//Jordan V. 1300520
+//Taras K. 6901204
+//Gianni T. 1938878
+//Sebastien S. 9500782
+//This is the Bipod header. This class makes bipod leg parts.
+
+
 #pragma once
 class Bipod
 {
